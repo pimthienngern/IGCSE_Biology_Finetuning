ec5401b6f9f885a4ddb414b4778c9339bd56471c
Pim Thien-ngern <pimthienngern@gmail.com>
2025-08-04 10:55:24 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ from transformers import pipeline
 print("Loading fine-tuned model...")
 qa = pipeline("text-classification", model="./model", tokenizer="./model")
 
-question = "advantages of GM plants"
+question = "" #put your question here
 print(f"Question: {question}")
 
 result = qa(question)
 label = result[0]["label"]
 score = result[0]["score"]
 
-if score < 0.1:
+if score < 0.01:
     print("Predicted answer: Sorry, I don't know the answer to that.")
 else:
     print(f"\nPredicted answer: {label}")
